c2bd1b459db1c204870bd5cc6d317593d0727576
lucianobs1 <lucianotetsu@gmail.com>
2025-01-17 12:23:10 -0300

    feature: controllers - SignIn and SignUp

--- src/application/useCases/SignUpUseCase.ts
+++ src/application/useCases/SignUpUseCase.ts
@@ -11,6 +11,7 @@ interface IInput {
 type IOutput = void;
 
 export class SignUpUseCase {
+  constructor(private readonly salt: number) {}
   async execute({ name, email, password }: IInput): Promise<IOutput> {
     const accountAlreadyExists = await prismaClient.account.findUnique({
       where: { email },
@@ -20,7 +21,7 @@ export class SignUpUseCase {
       throw new AccountAlreadyExists();
     }
 
-    const hashPassword = await hash(password, 8);
+    const hashPassword = await hash(password, this.salt);
 
     await prismaClient.account.create({
       data: {
