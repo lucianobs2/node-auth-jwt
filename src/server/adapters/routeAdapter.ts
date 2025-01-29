d9d477168e2ec5d45b575e0c3a17d84131f69db3
lucianobs1 <lucianotetsu@gmail.com>
2025-01-28 23:04:12 -0300

    feature: add role column in account

--- src/server/adapters/routeAdapter.ts
+++ src/server/adapters/routeAdapter.ts
@@ -3,28 +3,14 @@ import { IController } from '../../application/interfaces/IController';
 
 export function routeAdapter(controller: IController) {
   return async (request: Request, response: Response) => {
-    if (!request.metadata) {
-      console.log(request.metadata);
-      const { body, statusCode } = await controller.handle({
-        body: request.body,
-        params: request.params,
-      });
+    const { body, statusCode } = await controller.handle({
+      body: request.body,
+      params: request.params,
+      accountId: request.metadata?.accountId,
+    });
 
-      response.status(statusCode).json({
-        message: body,
-      });
-    } else {
-      console.log(request.metadata.accountId);
-
-      const { body, statusCode } = await controller.handle({
-        body: request.body,
-        params: request.params,
-        accountId: request.metadata.accountId,
-      });
-
-      response.status(statusCode).json({
-        message: body,
-      });
-    }
+    response.status(statusCode).json({
+      message: body,
+    });
   };
 }
